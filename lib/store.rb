9b4e1ce02b66728a48ec35a90a7f23c70fcac0e2
Nic <nic.ehmayer92@gmail.com>
2018-02-28 09:18:37 +0000

    Finished bonus custom validation for mens/womens apparel

--- lib/store.rb
+++ lib/store.rb
@@ -3,5 +3,15 @@ class Store < ActiveRecord::Base
 
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, minimum: 1 }
+  validate :store_must_carry_atleast_mens_or_womens_apparel
+
+  def store_must_carry_atleast_mens_or_womens_apparel
+    unless mens_apparel.present? || womens_apparel.present?
+      mens_apparel.present?
+        errors.add(:mens_apparel, "must be present if womens_apparel is not")
+      womens_apparel.present?
+        errors.add(:womens_apparel, "must be present if mens_apparel is not")
+    end
+  end
 
 end
